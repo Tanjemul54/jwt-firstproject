710eff9e5054ddcfae7e0995cbf1e3c548628f7d
Tanjemul54 <121612075+Tanjemul54@users.noreply.github.com>
2023-03-16 12:21:10 +0600

    First commit

---
+++ src/main/java/com/tjcompany/jwtfirstproject/JwtFirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.tjcompany.jwtfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtFirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtFirstprojectApplication.class, args);
+	}
+
+}
